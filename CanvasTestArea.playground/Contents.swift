c8e0543f74b5681bb596a25fc99a9f0d0b994125
Jessie Zhu <jezhu@lcs-mba3260.lcs.on.ca>
2017-09-28 11:05:23 -0400

    Made progress in learning how to draw with canvas.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -31,8 +31,8 @@ canvas.drawAxes()
 
 // Draw an ellipse in a different color at the centre of the canvas
 canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 100)
+canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 200)
 
-
-// This code is necessary to see the result in the Assistant Editor at right
+// Add a rectangle to the top of the screen
+canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
 PlaygroundPage.current.liveView = canvas.imageView
