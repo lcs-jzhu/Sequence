7c9c405aa704619344e53f13e163bd738127f9f2
Jessie Zhu <jezhu@lcs-mba3260.lcs.on.ca>
2017-09-30 12:00:37 -0400

    Got raise and rainbow done.

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -21,18 +21,63 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 500, height: 500)
 
-// Draw a circle at the origin with radius of 50 pixels
-canvas.drawEllipse(centreX: 0, centreY: 0, width: 50, height: 50)
+canvas.fillColor = Color.blue
+canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
+
 
 // Show where the origin is
 canvas.drawAxes()
+canvas.drawRectangle(centreX: 250, centreY: 250, width: 500, height: 500)
 
-// Draw an ellipse in a different color at the centre of the canvas
-canvas.fillColor = Color.green
-canvas.drawEllipse(centreX: 150, centreY: 250, width: 50, height: 200)
 
-// Add a rectangle to the top of the screen
-canvas.drawRectangle(centreX: 150, centreY: 400, width: 100, height: 50)
+canvas.lineColor = Color.yellow
+canvas.drawLine(fromX: 0, fromY: 500, toX: 100, toY: 0, lineWidth: 7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 200, toY: 0, lineWidth: 7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 300, toY: 0, lineWidth: 7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 400, toY: 0, lineWidth: 7)
+canvas.drawLine(fromX: 0, fromY: 500 , toX: 500, toY: 50, lineWidth:7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 150, lineWidth:7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 250, lineWidth:7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY: 350, lineWidth:7)
+canvas.drawLine(fromX: 0, fromY: 500, toX: 500, toY:450, lineWidth:7)
+canvas.fillColor = Color.yellow
+canvas.fillColor = Color.red
+canvas.drawShapesWithFill = false
+canvas.borderColor = Color.red
+canvas.defaultBorderWidth = 30
+
+
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 500, height: 500)
+
+canvas.drawShapesWithFill = false
+canvas.borderColor = Color.red
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 500, height: 500)
+canvas.borderColor = Color.orange
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 470, height: 470)
+canvas.borderColor = Color.yellow
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 440, height: 440)
+canvas.borderColor = Color.green
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 410, height: 410)
+canvas.borderColor = Color.blue
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 380, height: 380)
+canvas.borderColor = Color.purple
+canvas.defaultBorderWidth = 30
+canvas.drawEllipse(centreX: 250, centreY: 0, width: 350, height: 350)
+
+
+
+
+
+
+
+
+
+// Show the result
 PlaygroundPage.current.liveView = canvas.imageView
